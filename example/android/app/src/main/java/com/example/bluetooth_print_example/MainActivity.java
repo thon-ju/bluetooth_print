a85af47bd4037b592f86614d040dac3767220ccf
thon <thon.ju@gmail.com>
2022-10-26 15:22:07 +0800

    support flutter3.0.x

--- example/android/app/src/main/java/com/example/bluetooth_print_example/MainActivity.java
+++ example/android/app/src/main/java/com/example/bluetooth_print_example/MainActivity.java
@@ -1,13 +1,7 @@
 package com.example.bluetooth_print_example;
 
-import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-    GeneratedPluginRegistrant.registerWith(flutterEngine);
-  }
+
 }
